3410df7bb52f608fec8a93d50e38e4dc36d85b6e
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-09 15:27:27 +0200

    rename index.html in index.php :cat:

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div id="navbar">
       <ul>
         <li>
-          <a href="#">Accueil</a>
+          <a href="index.html">Accueil</a>
         </li>
         <li>
           <a href="#">Blog</a>
@@ -29,15 +29,16 @@
       <p>Bonjour à toi petit(e) Padawan et bienvenu sur mon site !
         Je me présente, Fanny, nouvelle dans le monde du code et de la programmation.
         Ancienne Historienne de l'Art j'y vois ici un tout autre moyen d'utiliser le développement web comme sauvegarde de la culture et de la connaissance.
-        Tu l'auras deviné petit(e) Padawan aguerri(e) je suis une amoureuse inconditionnelle de Github.</p>
+        Tu l'auras deviné petit(e) Padawan aguerri(e) je suis une amoureuse inconditionnelle de Github.
+        D'ailleurs mon pauvre chat en a fait les frais, il s'appelle Git (j'aime troller la vie).</p>
     </div>
 
     <footer>
       <ul>
         <li>
-          <a href="#">Accueil</a>
+          <a href="index.html">Accueil</a>
         </li>
-        <li>
+        <!-- <li>
           <a href="#">Blog</a>
         </li>
         <li>
@@ -45,10 +46,10 @@
         </li>
         <li>
           <a href="#">A propos</a>
-        </li>
-        <li>
+        </li> -->
+        <!-- <li>
           <a href="#">Mentions Légales</a>
-        </li>
+        </li> -->
       </ul>
     </footer>
 

a928ea3f8151b6805008b4e01d21d089fd355f23
Fanny Perret <fannyperretlombard@gmail.com>
2018-06-07 17:07:18 +0200

    add new pages in my portfolio and start to add some css :cat:

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Php Blog From Scratch</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300" rel="stylesheet">
   </head>
   <body>
 
-    <header>
+    <header id="header">
       <h1>Dans Ton Dev</h1>
     </header>
 
@@ -30,19 +32,19 @@
     <div id="presentation">
       <h2>Qui suis-je ?</h1>
       <img src="assets/img/presentation_me.jpg" alt="Portrait of me in Lisboa during a road-trip in 2017.">
-      <p>Bonjour à toi petit(e) Padawan et bienvenu sur mon site !
-        Je me présente, Fanny, nouvelle dans le monde du code et de la programmation.
-        Ancienne Historienne de l'Art j'y vois ici un tout autre moyen d'utiliser le développement web comme sauvegarde de la culture et de la connaissance.
+      <p>Bonjour à toi petit(e) Padawan et bienvenu(e) sur mon site !
+        Je me présente, Moi, petite nouvelle dans le monde du code et de la programmation.
+        Ancienne Historienne de l'Art j'y vois ici un tout autre moyen d'utiliser le développement web comme sauvegarde de la culture et de la connaissance (quelle noblesse d'âme...).
         Tu l'auras deviné petit(e) Padawan aguerri(e) je suis une amoureuse inconditionnelle de Github.
         D'ailleurs mon pauvre chat en a fait les frais, il s'appelle Git (j'aime troller la vie).</p>
     </div>
 
-    <footer>
+    <footer id="footer">
       <ul>
         <li>
           <a href="index.php">Accueil</a>
         </li>
-        <!-- <li>
+        <li>
           <a href="#">Blog</a>
         </li>
         <li>
@@ -51,9 +53,9 @@
         <li>
           <a href="#">A propos</a>
         </li> -->
-        <!-- <li>
-          <a href="#">Mentions Légales</a>
-        </li> -->
+        <li>
+          <a href="contact.php">Contact</a>
+        </li>
       </ul>
     </footer>
 

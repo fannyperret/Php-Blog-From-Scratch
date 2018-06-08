4890303ebea1ae151696d3e574e147a9f0e97510
Fanny Perret <fannyperretlombard@gmail.com>
2018-06-08 11:45:16 +0200

    add favicon to my pages :cat:

--- actions/validate_form.php
+++ actions/validate_form.php
@@ -5,6 +5,7 @@
     <title>Php Blog From Scratch</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="96x96" href="../assets/img/favicon-96x96.png">
   </head>
   <body>
 

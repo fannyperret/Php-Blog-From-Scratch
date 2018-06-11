4f74783b8a12f3db7bf7a00fb8a2a619601894f5
Fanny Perret <fannyperretlombard@gmail.com>
2018-06-11 14:05:59 +0200

    add function date to the header and change css file :cat:

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,21 @@
 <header id="header">
+  <div class="today">
+    <?php
+    // indication of days and some funny text added
+    function sayTheDay() {
+      $tab_days = array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+      $date = $tab_days[date('w', mktime(0,0,0,date('m'),date('d'),date('Y')))];
+      if ($date == 'Lundi') {
+        echo "Courage aujourd'hui c'est " . $date . "...";
+      } elseif ($date == 'Samedi' | 'Dimanche') {
+        echo "Youpi c'est le weekend !";
+      } else {
+        echo "Aujourd'hui nous sommes " . $date . " !";
+      }
+    }
+    sayTheDay();
+     ?>
+  </div>
   <h1>Dans Ton Dev</h1>
   <p>"Site actuellement en construction"</p>
 </header>

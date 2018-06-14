c99e41910cf5db1e2b58582bdcf90071e7809cdd
Fanny <fanny@MacBook-Pro-de-Adrien.local>
2018-06-14 17:36:02 +0200

    restructuration of mobile device and some css changes :cat:

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 <title>Fanny Perret</title>
 <link rel="stylesheet" href="assets/css/style.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300" rel="stylesheet">
--- pages/home.php
+++ pages/home.php
@@ -4,6 +4,11 @@
   <div class="main">
     <h2>Title Heading</h2>
     <h3>Title description</h3>
+    <p>Bonjour à toi petit(e) Padawan et bienvenu(e) sur mon site !
+      Je me présente, Moi, petite nouvelle dans le monde du code et de la programmation.
+      Ancienne Historienne de l'Art j'y vois ici un tout autre moyen d'utiliser le développement web comme sauvegarde de la culture et de la connaissance (quelle noblesse d'âme...).
+      Ce site sera l'expression même de mon évolution, une sorte de "laboratoire du code".
+      Sinon ? J'aime beaucoup utiliser les commandes git... A tel point que mon chat en porte le nom...</p>
   </div>
 
   <div class="right_side">
